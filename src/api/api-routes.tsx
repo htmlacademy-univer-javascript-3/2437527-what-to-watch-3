5d75cc0f4069e8b1a664f0808f70ec137ad7c1af
a.burgart02 <a.burgart02@gmail.com>
2024-01-03 17:03:05 +0500

    Fix

--- src/api/api-routes.tsx
+++ src/api/api-routes.tsx
@@ -1,12 +1,12 @@
-export enum ApiRoutes {
-  Main = '/',
-  Films = '/films',
-  Film = (id: number | string) => `/films/${id}`,
-  SimilarFilms = (id: number | string) => `/films/${id}/similar`,
-  Promo = '/promo',
-  Reviews = (id: number | string) => `/comments/${id}`,
-  Login = '/login',
-  Logout = '/logout',
-  Favorites = '/favorite',
-  PostFavorite = (filmId: number | string, status: number | string) => `/favorite/${filmId}/${status}`
-}
+export const ApiRoutes = {
+  Main: '/',
+  Films: '/films',
+  Film: (id: number | string) => `/films/${id}`,
+  SimilarFilms: (id: number | string) => `/films/${id}/similar`,
+  Promo: '/promo',
+  Reviews: (id: number | string) => `/comments/${id}`,
+  Login: '/login',
+  Logout: '/logout',
+  Favorites: '/favorite',
+  PostFavorite: (filmId: number | string, status: number | string) => `/favorite/${filmId}/${status}`
+} as const;
